9aa6835063d37b4784a6c0d6a1a4076b73ed128e
chusheva_olga <chusheva.olya@yandex.ru>
2023-06-27 23:56:40 +0300

    добавлен view с авторизацией пользователя

---
+++ ImageFeed/ServiceAccessLayer/OAuth2/OAuth2TokenStorage.swift
@@ -0,0 +1,13 @@
+//
+//  OAuth2TokenStorage.swift
+//  ImageFeed
+//
+//  Created by Ольга Чушева on 19.06.2023.
+//
+
+import Foundation
+
+final class OAuth2TokenStorage {
+    var token: String? 
+}
+
