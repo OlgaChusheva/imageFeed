f71176e558ef0b96a205977a307becfd2fbb61a6
chusheva_olga <chusheva.olya@yandex.ru>
2023-07-02 16:40:31 +0300

    избавились от гонки при входе в приложение

--- ImageFeed/ServiceAccessLayer/OAuth2/OAuth2Service.swift
+++ ImageFeed/ServiceAccessLayer/OAuth2/OAuth2Service.swift
@@ -3,31 +3,55 @@ import Foundation
 final class OAuth2Service {
     static let shared = OAuth2Service()
     private let urlSession = URLSession.shared
-    private (set) var authToken: String? {
-        get {
-            return OAuth2TokenStorage().token
+    private var task: URLSessionTask?
+    private var lastCode: String?
+    
+    /*    private (set) var authToken: String? {
+     get {
+     return OAuth2TokenStorage().token
+     }
+     set {
+     OAuth2TokenStorage().token = newValue
+     } }
+     */
+    
+    
+    func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void ) {
+        assert(Thread.isMainThread)
+        if lastCode == code {return}
+        task?.cancel()
+        lastCode = code
+        
+        let request = makeRequest(code: code)
+        let task = urlSession.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async { [self] in
+                let task = self.object(for: request) { [weak self] result in
+                    guard let self = self else { return }
+                    switch result {
+                    case .success(let body):
+                        completion(.success(body.accessToken))
+                        self.task = nil
+                    case .failure(let error):
+                        completion(.failure(error))
+                        self.lastCode = nil
+                    }
+                }
+            }
         }
-        set {
-            OAuth2TokenStorage().token = newValue
-        } }
-    func fetchOAuthToken(
-        _ code: String,
-        completion: @escaping (Result<String, Error>) -> Void ){
-            let request = authTokenRequest(code: code)
-            let task = object(for: request) { [weak self] result in
-                guard let self = self else { return }
-                switch result {
-                case .success(let body):
-                    let authToken = body.accessToken
-                    self.authToken = authToken
-                    completion(.success(authToken))
-                case .failure(let error):
-                    completion(.failure(error))
-                } }
-            task.resume()
-        }
+        
+        
+        
+        self.task = task
+        task.resume()
+    }
+    
+    private func makeRequest(code: String) -> URLRequest {
+    guard let url = URL(string: "https://unsplash.com") else  {fatalError("Failed to create URL")}
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    return request
+    }
 }
-
 
 extension OAuth2Service {
     private func object(
